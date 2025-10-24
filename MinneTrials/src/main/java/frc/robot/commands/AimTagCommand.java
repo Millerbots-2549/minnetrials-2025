d246c16bdd46b4e9f470b0a139d3d00b2b3f7f8a
Croix <ckos2801@mpsedu.org>
2025-10-24 13:15:48 -0500

    feat: Implement dual PID for angle and distance control in AimTagCommand

--- MinneTrials/src/main/java/frc/robot/commands/AimTagCommand.java
+++ MinneTrials/src/main/java/frc/robot/commands/AimTagCommand.java
@@ -10,35 +10,42 @@ import frc.robot.subsystems.DriveSubsystem;
 public class AimTagCommand extends Command {
 
     private final DriveSubsystem drive;
-    private final PIDController pid;
+    private final PIDController anglepid;
+    private final PIDController distancepid;
 
     public AimTagCommand(DriveSubsystem drive) {
         this.drive = drive;
-        this.pid = new PIDController(VisionConstants.kP, VisionConstants.kI, VisionConstants.kD);
-        pid.setTolerance(VisionConstants.kToleranceDegrees);
+        this.anglepid = new PIDController(VisionConstants.anglekP, VisionConstants.anglekI, VisionConstants.anglekD);
+        this.distancepid = new PIDController(VisionConstants.distancekP, VisionConstants.distancekI, VisionConstants.distancekD);
+        anglepid.setTolerance(VisionConstants.kToleranceDegrees);
         addRequirements(drive);
     }
 
     @Override
     public void initialize() {
-        pid.reset();
+        anglepid.reset();
+        distancepid.reset();
 
         if (DriverStation.getAlliance().isPresent()) {
             if (DriverStation.getAlliance().get() == DriverStation.Alliance.Red) {
-                LimelightHelpers.SetFiducialIDFiltersOverride("", new int[]{6, 7});
+                LimelightHelpers.SetFiducialIDFiltersOverride(VisionConstants.LimelightName, new int[]{6, 7});
             } else if (DriverStation.getAlliance().get() == DriverStation.Alliance.Blue) {
-                LimelightHelpers.SetFiducialIDFiltersOverride("", new int[]{4, 5});
+                LimelightHelpers.SetFiducialIDFiltersOverride(VisionConstants.LimelightName, new int[]{4, 5});
             }
         }
     }
 
     @Override
     public void execute() {
-        double tx = LimelightHelpers.getTX("");
-        double output = pid.calculate(tx, VisionConstants.TARGET_YAW);
+        double tx = LimelightHelpers.getTX(VisionConstants.LimelightName);
+        double angleoutput = anglepid.calculate(tx, VisionConstants.TARGET_YAW);
+
+        double ty = LimelightHelpers.getTY(VisionConstants.LimelightName);
+        double distanceoutput = distancepid.calculate(ty, VisionConstants.TARGET_DISTANCE);
+        
 
         // Rotate robot to aim at target
-        drive.arcadeDrive(0, -output);
+        drive.arcadeDrive(distanceoutput, -angleoutput);
     }
 
     @Override
@@ -48,6 +55,6 @@ public class AimTagCommand extends Command {
 
     @Override
     public boolean isFinished() {
-        return pid.atSetpoint();
+        return anglepid.atSetpoint();
     }
 }
