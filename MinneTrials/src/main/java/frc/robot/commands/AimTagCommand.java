c846a5c1d0212eee73c64347ebf53978de4dc1fd
Croix <ckos2801@mpsedu.org>
2025-10-23 22:39:33 -0500

    feat: Dynamically set Limelight fiducial filters based on alliance

--- MinneTrials/src/main/java/frc/robot/commands/AimTagCommand.java
+++ MinneTrials/src/main/java/frc/robot/commands/AimTagCommand.java
@@ -2,43 +2,52 @@ package frc.robot.commands;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.DriverStation;
 import frc.robot.LimelightHelpers;
 import frc.robot.Constants.VisionConstants;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class AimTagCommand extends Command {
 
-  private final DriveSubsystem drive;
-  private final PIDController pid;
+    private final DriveSubsystem drive;
+    private final PIDController pid;
 
-  public AimTagCommand(DriveSubsystem drive) {
-    this.drive = drive;
-    this.pid = new PIDController(VisionConstants.kP, VisionConstants.kI, VisionConstants.kD);
-    pid.setTolerance(VisionConstants.kToleranceDegrees);
-    addRequirements(drive);
-  }
+    public AimTagCommand(DriveSubsystem drive) {
+        this.drive = drive;
+        this.pid = new PIDController(VisionConstants.kP, VisionConstants.kI, VisionConstants.kD);
+        pid.setTolerance(VisionConstants.kToleranceDegrees);
+        addRequirements(drive);
+    }
 
-  @Override
-  public void initialize() {
-    pid.reset();
-  }
+    @Override
+    public void initialize() {
+        pid.reset();
 
-  @Override
-  public void execute() {
-    double tx = LimelightHelpers.getTX("");
-    double output = pid.calculate(tx, VisionConstants.TARGET_YAW);
+        if (DriverStation.getAlliance().isPresent()) {
+            if (DriverStation.getAlliance().get() == DriverStation.Alliance.Red) {
+                LimelightHelpers.SetFiducialIDFiltersOverride("", new int[]{6, 7});
+            } else if (DriverStation.getAlliance().get() == DriverStation.Alliance.Blue) {
+                LimelightHelpers.SetFiducialIDFiltersOverride("", new int[]{4, 5});
+            }
+        }
+    }
 
-    // Rotate robot to aim at target
-    drive.arcadeDrive(0, -output);
-  }
+    @Override
+    public void execute() {
+        double tx = LimelightHelpers.getTX("");
+        double output = pid.calculate(tx, VisionConstants.TARGET_YAW);
 
-  @Override
-  public void end(boolean interrupted) {
-    drive.arcadeDrive(0, 0);
-  }
+        // Rotate robot to aim at target
+        drive.arcadeDrive(0, -output);
+    }
 
-  @Override
-  public boolean isFinished() {
-    return pid.atSetpoint();
-  }
+    @Override
+    public void end(boolean interrupted) {
+        drive.arcadeDrive(0, 0);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return pid.atSetpoint();
+    }
 }
